e2306fa0eb3ccc907cd4296c15c575e577443b7c
JinaKim77 <G00353420@gmit.ie>
2021-01-21 16:10:21 +0000

    Styled navigation using styled-components

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -5,18 +5,22 @@ import Covid19 from '../Routes/Covid19';
 import Search from '../Routes/Search';
 import MersAndSars from '../Routes/MersAndSars';
 import Smallpox from '../Routes/Smallpox';
+import Header from '../Header';
 
 function Router(){
     return (
         <Browser>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/covid19" component={Covid19} />
-            <Route path="/search" component={Search} />
-            <Route path="/mersandsars" component={MersAndSars} />
-            <Route path="/smallpox" component={Smallpox} />
-            <Redirect from="*" to="/" />  
+          <>
+            <Header />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/covid19" component={Covid19} />
+              <Route path="/search" component={Search} />
+              <Route path="/mersandsars" component={MersAndSars} />
+              <Route path="/smallpox" component={Smallpox} />
+              npm i styled-reset<Redirect from="*" to="/" />  
           </Switch>
+          </>
         </Browser>
     )
     
--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,61 @@
 import React from 'react';
+import styled from 'styled-components';
+import {Link, withRouter} from 'react-router-dom';
 
-export default ()=>(
-    <header>
-        <ul>
-            <li>
-                <a href="/">Home</a>
-            </li>
-            <li>
-                <a href="/covid19">Covid-19</a>
-            </li>
-            <li>
-                <a href="/mersandsars">Mers and Sars</a>
-            </li>
-            <li>
-                <a href="/smallpox">Smallpox</a>
-            </li>
-            <li>
-                <a href="/search">Search</a>
-            </li>
-        </ul>
-    </header>
+const List = styled.ul`
+    display:flex;
+`;
+
+const Header = styled.header`
+    color : white;
+    position:fixed;
+    top:0;
+    left:0;
+    width:100%;
+    height:50px;
+    display:flex;
+    align-items:center;
+    padding:0px 10px;
+    background-color:rgba(20,20,20,0.8);
+    z-index:10;
+    box-shadow:0px 1px 5px 2px rgba(0,0,0,0.8);
+`;
+
+const Item = styled.li`
+    width:120px;
+    height:50px;
+    text-align:center;
+    border-bottom:5px solid ${props => props.current ? "#8B0000":"transparent"};
+    transition : border-bottom 0.5s ease-in-out;
+`;
+
+const SLink = styled(Link)`
+    height:50px;
+    display:flex;
+    align-items:center;
+    justify-content:center;
+`;
+
+export default withRouter(({ location : {pathname} })=>(
+        <Header>
+            <List>
+                <Item current={pathname === "/"}>
+                    <SLink to="/">Home </SLink>
+                </Item>
+                <Item current={pathname === "/covid19"}>
+                    <SLink to="/covid19">Covid-19 </SLink>
+                </Item>
+                <Item current={pathname === "/mersandsars"}>
+                    <SLink to="/mersandsars">Mers and Sars </SLink>
+                </Item>
+                <Item current={pathname === "/smallpox"}>
+                    <SLink to="/smallpox">Smallpox </SLink>
+                </Item>
+                <Item current={pathname === "/search"}>
+                    <SLink to="/search">Search </SLink>
+                </Item>
+            </List>
+        </Header>
+    )
+
 )
