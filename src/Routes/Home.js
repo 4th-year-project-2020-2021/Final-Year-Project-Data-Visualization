9cdf7c9f4953aee0fcc7e979ffb4d8de5c724c05
gracekeane <gracekeane82@gmail.com>
2021-02-18 18:10:13 +0000

    Small changes to home

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,12 +1,22 @@
+import { color } from 'd3';
 import React from 'react'
   
     function Home() {
         return (
-          <div>
-            <h1 fontSize="6px">Data Visualization & Analysis</h1>
-          </div>
-        )
-      }
+          <div
+            style={{
+            textAlign: "center",
+            fontSize:"40px",
+            fontFamily: "Nanum Gothic",
+            color: "white"
+         }}
+        >
 
+          <h2>Data Visualization & Analysis</h2>
+
+          
+          </div>
+        );
+    }
 
 export default Home;
