232d3c41a9adfa3ca76d63fc2fb68713176d8554
gracekeane <gracekeane82@gmail.com>
2021-03-12 17:46:15 +0000

    Added drop downs and table for covid cases

---
+++ src/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Map() {
+    return (
+        <div className="InteractiveMap">
+            <h1> Interactive map goes here </h1>
+        </div>
+    )
+}
+
+export default Map
