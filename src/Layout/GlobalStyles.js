b440101d2ed6267ba5022fda5f1f73b9ca8a0e57
JinaKim77 <G00353420@gmit.ie>
2021-03-12 18:31:01 +0000

    Removed global styling for the header

--- src/Layout/GlobalStyles.js
+++ src/Layout/GlobalStyles.js
@@ -3,19 +3,6 @@ import reset from 'styled-reset';
 
 const globalStyles = createGlobalStyle`
     ${reset};
-    body{
-        background-color: #eaeaea;
-    }
-
-    .ui.menu {
-        background-image: linear-gradient(
-            135deg, 
-            rgb(24, 42, 115) 0%, 
-            rgb(32, 138, 174) 69%, 
-            rgb(32, 167, 172) 89%
-        )
-    };
-
     svg {
         display: inline-block;
         vertical-align: middle;
