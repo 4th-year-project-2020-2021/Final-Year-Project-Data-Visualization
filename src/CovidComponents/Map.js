c5d2774c1affee26a5693ab19012162c50a3ad80
gracekeane <gracekeane82@gmail.com>
2021-03-19 12:04:07 +0000

    Started interactive map, not fully displaying yet - needs more work

--- src/CovidComponents/Map.js
+++ src/CovidComponents/Map.js
@@ -1,19 +1,20 @@
-/*import React from "react";
-import { MapContainer } from "react-leaflet";
+import React from "react";
+import { MapContainer as LeafletMap, TileLayer, useMap } from 'react-leaflet'
 import "../css/styling.css";
-import "leaflet/dist/leaflet.css";
+import "leaflet/dist/leaflet";
 
-function interactiveMap({ countries, casesType, center, zoom }) {
-    return (
-        <div className="map">
-          <LeafletMap center={center} zoom={zoom}>
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            />
-          </LeafletMap>
-        </div>
-      );
-    }
+function MapContainer({ center, zoom }) {
+  return (
+      <div className="map">
+        <LeafletMap center={center} zoom={zoom}>
 
-export default interactiveMap;*/
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+          
+        </LeafletMap>
+      </div>
+  );
+}
+export default MapContainer;
