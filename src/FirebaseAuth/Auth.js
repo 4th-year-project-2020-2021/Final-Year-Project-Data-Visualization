2fb3a68d1d7cf36ea18dbfec287a93df1eb059d9
JinaKim77 <G00353420@gmit.ie>
2021-03-25 18:38:27 +0000

    Delete fortawesome package

--- src/FirebaseAuth/Auth.js
+++ src/FirebaseAuth/Auth.js
@@ -1,9 +1,9 @@
 import React,{useState} from 'react';
 import firebase, { firebaseInstance ,authService } from "../FirebaseAuth/firebase";
 import AuthForm from './AuthForm';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle,faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//import { faGoogle,faGithub } from '@fortawesome/free-brands-svg-icons';
+//import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 const Auth = () => {
     const onSocialClick = async (e) => {
@@ -22,16 +22,18 @@ const Auth = () => {
 
      <div className="authContainer">
      <h3>Welcome</h3>
-      <FontAwesomeIcon
+      {/*<FontAwesomeIcon
         icon={faCoffee}
         color={"#04AAFF"}
         size="3x"
         style={{ marginBottom: 30 }}
-      />
+      />*/}
        <AuthForm />
         <div className="authBtns">
-            <button onClick={onSocialClick} name="google" className="authBtn">Continue with Google <FontAwesomeIcon icon={faGoogle} /></button>
-            <button onClick={onSocialClick} name="github" className="authBtn">Continue with Github <FontAwesomeIcon icon={faGithub} /></button>
+            {/*<button onClick={onSocialClick} name="google" className="authBtn">Continue with Google <FontAwesomeIcon icon={faGoogle} /></button>
+            <button onClick={onSocialClick} name="github" className="authBtn">Continue with Github <FontAwesomeIcon icon={faGithub} /></button>*/}
+            <button onClick={onSocialClick} name="google" className="authBtn">Continue with Google </button>
+            <button onClick={onSocialClick} name="github" className="authBtn">Continue with Github </button>
         </div>
      </div>
     );
