6e808d3fdef097cf397ea75339e371e07c926268
gracekeane <gracekeane82@gmail.com>
2021-04-29 21:30:03 +0100

    Updated graph - needs more work

--- src/Routes/Stats.js
+++ src/Routes/Stats.js
@@ -2,14 +2,18 @@ import React, { useEffect, useState } from 'react'
 import "../css/styling.css";
 import ProgressGraph from "../CovidComponents/ProgressGraph";
 import axios from 'axios';
+import { axisLeft } from 'd3-axis';
 
 function Stats() {
     const [covidProgress, setCovidProgress] = useState({});
+    const [days, setDays] = useState({});
+    const [country, setCountry] = useState('');
+    const [coronaCountArray, setCoronaCountArray] = useState([]);
 
     useEffect(() => {
 
         // Calling api
-        axios.get(`https://api.covid19api.com/summary`)
+        axios.get("https://api.covid19api.com/summary")
             .then(res => {
                 if (res.status === 200) {
                     setCovidProgress(res.data);
@@ -21,20 +25,62 @@ function Stats() {
             })
     }, []);
 
+    const formatDate = (date) => {
+        const d = new Date(date);
+        const year = d.getFullYear();
+        const month = `0${d.getMonth() + 1}`.slice(-2);
+        const _date = d.getDate();
+        return `${year}-${month}-${date}`;
+    }
+
+    const countryHandler = (e) => {
+        setCountry(e.target.value);
+        const d = new Date();
+        const to = formatDate(d);
+        const from = formatDate(d.setDate(d.getDate() - 7));
+
+        console.log(from, to);
+        getCoronaProgress(e.target.value, from, to);
+    }
+
+    const daysHandler = (e) => {
+        setDays(e.target.value);
+    }
+
+    const getCoronaProgress = (countrySlug, from, to) => {
+        axios.get(`/country/${countrySlug}/status/confirmed?from=${from}T00:00:00Z&to=${to}T00:00:00Z`)
+            .then(res => {
+                console.log(res);
+                const yAxisCoronaCount = res.data.map(d => d.Cases);
+                setCoronaCountArray(yAxisCoronaCount)
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     return (
-        <div>
+        <div className="App">
             <div>
                 <div>
-                    <select>
+                    <select value={country} onChange={countryHandler}>
                         {
                             covidProgress.Countries && covidProgress.Countries.map(country =>
-                                <option value={country.Slug}>{country.Country}</option>
+                                <option key={country.Slug} value={country.Slug}>{country.Country}</option>
                             )
                         }
                     </select>
+                    <select value={days} onChange={daysHandler}>
+                        <option value="7">Last 7 Days</option>
+                        <option value="30">Last 30 Days</option>
+                        <option value="90">Last 90 Days</option>
+
+                    </select>
                 </div>
+                <ProgressGraph
+                yAxis={coronaCountArray}
+            />
             </div>
-            <ProgressGraph />
         </div>
     );
 }
