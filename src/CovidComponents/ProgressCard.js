c6a94dca4ef43cfcde1d521cd74514fca072a63e
gracekeane <gracekeane82@gmail.com>
2021-05-01 11:29:27 +0100

    Added progress card

---
+++ src/CovidComponents/ProgressCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../css/styling.css";
+
+const ProgressCard = (props) => {
+    return(
+        <div className="app__stats">
+            
+        </div>
+    )
+}
+
+export default  ProgressCard
