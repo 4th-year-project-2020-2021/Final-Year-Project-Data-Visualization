93f2ea0b2cdb4fa0deedce7cdbb4e3a84211ab47
gracekeane <gracekeane82@gmail.com>
2021-05-01 19:32:07 +0100

    Finished stats - need to style and fix recovery overlay

--- src/ProgressComponent/CountryPicker/CountryPicker.jsx
+++ src/ProgressComponent/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import { FormControl, NativeSelect } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { fetchCountries } from '../../ProgressComponent';
 
-const CountryPicker = () => {
+const CountryPicker = ({ handleCountryChange }) => {
+const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setFetchedCountries(await fetchCountries())
+        }
+
+        fetchAPI();
+    }, [setFetchedCountries]);
     return (
-        <h1>Cards</h1>
+        <FormControl>
+            <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+                <option value="">Global</option>
+                {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
     )
 }
 
